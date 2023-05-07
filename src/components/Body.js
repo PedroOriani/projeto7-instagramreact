1ec01fc284599596ee9fc72650e6e7547a5f7139
Pedro Oriani <pedro.oriani@gmail.com>
2023-05-07 18:51:52 -0300

    first commit

---
+++ src/components/Body.js
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom";
+
+import Stories from "./Stories";
+
+export default function Body(){
+    return(
+        <div class='corpo'>
+            <Stories />
+        </div>
+    );
+}
---
+++ src/components/Story.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom";
+
+export default function Story(){
+    return(
+
+    );
+
+}
