21ecb7214151076c6c674332866fbe4c25e4847a
Pedro Oriani <pedro.oriani@gmail.com>
2023-05-08 01:20:42 -0300

    Alterado img e name

--- src/components/User.js
+++ src/components/User.js
@@ -1,13 +1,33 @@
+import { setupIonicReact } from "@ionic/react";
 import { ReactDOM } from "react"
+import { useState } from "react";
 
 export default function User(){
+
+  let[userIcon, setUserIcon] = useState ("assets/img/catanacomics.svg")
+  let[userName, setUserName] = useState ('catanacomics')
+
+  function newIcon(){
+    let newIcon = prompt('Novo Icone de Usuário')
+    if(newIcon){
+      setUserIcon(newIcon)
+    }
+  }
+
+  function newName(){
+    let newName = prompt('Novo nome')
+    if(newName){
+      setUserName(newName)
+    }
+  }
+
   return(
     <div class="usuario">
-      <img data-test="profile-image" src="assets/img/catanacomics.svg" alt="imagem de perfil" />
+      <img onClick={newIcon} data-test="profile-image" src={userIcon} alt="imagem de perfil" />
       <div class="texto">
         <span>
-          <strong data-test="name" >catanacomics</strong>
-          <ion-icon data-test="edit-name" name="pencil"></ion-icon>
+          <strong data-test="name" >{userName}</strong>
+          <ion-icon onClick={newName} data-test="edit-name" name="pencil"></ion-icon>
         </span>
       </div>
     </div>
